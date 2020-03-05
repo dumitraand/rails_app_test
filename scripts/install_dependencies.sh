019b5812c549d0674ea1fb32a3bccfbf2aea59d4
Andrei Dumitra <andrei.dumitra@sparktech.ro>
2020-03-05 16:55:22 +0200

    fix scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,9 +3,11 @@
 bundler_installed=$(gem list -i bundler)
 if [[ $bundler_installed == true ]]; then
   echo "gem: --no-rdoc --no-ri" > ~/.gemrc
-  cd /tmp/rails-app && bundle install
+  cd /tmp/rails-app
+  bundle install
 else
   echo "gem: --no-rdoc --no-ri" > ~/.gemrc
   gem install bundler
-  cd /tmp/rails-app && bundle install
+  cd /tmp/rails-app
+  bundle install
 fi
